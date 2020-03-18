2fcb397387dda6749d5d3532a46713c293e74f69
guissmejia <guissmejia@gmail.com>
2020-03-18 13:13:15 -0500

    Differences between jsx and React.createElement

--- src/index.js
+++ src/index.js
@@ -13,10 +13,25 @@ import ReactDOM from 'react-dom';
 // const jsx = <h1>Hello Guiselle!!</h1>;
 // const element = React.createElement('a', {href: 'https://platzi.com'}, 'Ir a Platzi')
 
-const name = 'Guiss';
 // const element = React.createElement('h1', {}, `Hola soy ${name}`);
-const jsx = <h1>Hola soy {name}</h1>
 
+//Diferencias entre jsx y React.createElement
+const name = 'Guiselle';
+const jsx = (
+  <div>
+    <h1>Hola soy {name}</h1>
+    <p>Soy Desarrolladora Web</p>
+  </div>
+);
+
+
+const element = React.createElement(
+  'div',
+  {},
+  '',
+  React.createElement('h1', {}, 'Hola soy Guiselle'),
+  React.createElement('p', {}, 'Soy Desarrolladora Web')
+);
 const container = document.getElementById('app');
 
 ReactDOM.render(jsx, container);
